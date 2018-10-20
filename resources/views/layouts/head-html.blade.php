9a0b832cc110a18d6577e718a9a3eb7e054ce412
Greg <duncan4210@gmail.com>
2018-10-20 22:52:34 +0200

    broken
    Something is not working anymore

--- resources/views/layouts/head-html.blade.php
+++ resources/views/layouts/head-html.blade.php
@@ -6,14 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{asset('/css/app.css')}}">
-    <title>{{config('app.name', 'Univerbal')}}</title>
+    <title>{{ config('app.name', 'Univerbal') }}</title>
 </head>
 
 <body>
     <div class="container">
         @yield('content')
     </div>
-    <script src="{{asset('/js/app.js')}}"></script>
+    <script src="{{ asset('/js/app.js') }}"></script>
 </body>
 
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix
+    .disableNotifications()
+    .js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .browserSync({
         host: "127.0.0.1",
