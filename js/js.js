73dd0db4ea02884764cca55254863d69be38f40a
Nathaliemug <42577410+Nathaliemug@users.noreply.github.com>
2018-10-24 10:53:26 +0200

    VERSION MATERIALIZE

--- js/js.js
+++ js/js.js
@@ -1,20 +1,14 @@
 $(function () {
-    var icons = {
-        header: "ui-icon-circle-arrow-e",
-        activeHeader: "ui-icon-circle-arrow-s"
-    };
-    $("#accordion").accordion({
-        icons: icons, active: false,
-        collapsible: true  ,
-        heightStyle: "content" 
-    });
-    $("#toggle").button().on("click", function () {
-        if ($("#accordion").accordion("option", "icons")) {
-            $("#accordion").accordion("option", "icons", null);
-        } else {
-            $("#accordion").accordion("option", "icons", icons);
-        }
-    });
+   $(document).ready(function(){
+        $('.collapsible').collapsible();
+      });
     
 });
 
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.collapsible');
+//     var instances = M.Collapsible.init(elems, options);
+// console.log(elem);
+
+//   });
