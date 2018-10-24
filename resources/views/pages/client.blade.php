8b6e6cae0046630f067d20a7872e1c033212f0da
bertholdberthi <43133806+bertholdberthi@users.noreply.github.com>
2018-10-24 12:43:15 +0200

    jh,rjh

--- resources/views/pages/client.blade.php
+++ resources/views/pages/client.blade.php
@@ -1,7 +1,18 @@
 @extends('layouts.head-html')
 
 @section('content')
-<div class="#app">
-    <h1>Client page</h1>
-</div>
-@endsection
+< <nav class="nav bar nav-inverse nav-fixed-top" id="my-navbar">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="navbar-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a href="#" class="navbar-brand"><i class="fas fa-hourglass-half"></i></a>
+            <a href="#" class="navbar-brand"><i class="far fa-calendar-check"></i></a>
+            <a href="#" class="navbar-brand"><i class="fas fa-history"></i></a>
+        </div>
+    </div>
+    </nav>
+    @endsection
