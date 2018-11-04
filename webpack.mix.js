b967c40c592362171a6a084cd2a7aebc0beeba14
Greg <duncan4210@gmail.com>
2018-11-04 10:41:59 +0100

    forgot webmix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,15 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+mix
+    .disableNotifications()
+    .copyDirectory('resources/fonts', 'public/fonts')
+    .copyDirectory('resources/img', 'public/img')
+    .js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .browserSync({
+        proxy: '127.0.0.1:8000',
+        // proxy: 'localhost',
+        port: '3000',
+        browser: 'chrome',
+    });
