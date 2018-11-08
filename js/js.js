6f64217447bbba83b0036e37cc02c33f6cb38e3a
bertholdberthi <43133806+bertholdberthi@users.noreply.github.com>
2018-11-08 10:13:00 +0100

    init

--- js/js.js
+++ js/js.js
@@ -1,11 +1,3 @@
-// $(function () {
-//    $(document).ready(function(){
-//         $('.collapsible').collapsible();
-//       });
-    
-// });
-
-//debut ajout-mission
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems, {
@@ -16,31 +8,3 @@ document.addEventListener('DOMContentLoaded', function() {
 console.log(elem);
 
   });
-
-  document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, options);
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.datepicker');
-    var instances = M.Datepicker.init(elems, options);
-});
-
-
-
-$(document).ready(function () {
-    $('select').material_select();
-});
-
-(function($){
-  $(function(){
-    // Plugin initialization
-    $('select').not('.disabled').formSelect();
-  }); 
-})(jQuery); // end of jQuery name space
-
-  $(document).ready(function() {
-    M.updateTextFields();
-  });
-  //fin ajout-mission
