93dbb19fc5cc6e09cc05a518870de47cc04047ff
Greg <duncan4210@gmail.com>
2018-11-10 16:46:18 +0100

    Config DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,28 +13,28 @@ class DatabaseSeeder extends Seeder
     {
 
         // Disabled all mass assignements protection
-        // App\User::unguard();
-        // App\Organisation::unguard();
-        // App\Profile::unguard();
-        // App\Adresse::unguard();
-        // App\Langue::unguard();
+        App\User::unguard();
+        App\Organisation::unguard();
+        App\Profile::unguard();
+        App\Adresse::unguard();
+        App\Langue::unguard();
         App\Country::unguard();
 
-
-        // $this->call(LaratrustSeeder::class);
-        // $this->call(UsersTableSeeder::class);
-        // $this->call(OrganisationsTableSeeder::class);
-        // $this->call(AdressesTableSeeder::class);
-        // $this->call(LangueTableSeeder::class);
+        // Import all seeders
+        $this->call(LaratrustSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(OrganisationsTableSeeder::class);
+        $this->call(AdressesTableSeeder::class);
+        $this->call(LangueTableSeeder::class);
         $this->call(CountriesTableSeeder::class);
 
 
         // Enabled all mass assignements protection
-        // App\User::reguard();
-        // App\Organisation::reguard();
-        // App\Profile::reguard();
-        // App\Adresse::reguard();
-        // App\Langue::reguard();
+        App\User::reguard();
+        App\Organisation::reguard();
+        App\Profile::reguard();
+        App\Adresse::reguard();
+        App\Langue::reguard();
         App\Country::reguard();
     }
 }
